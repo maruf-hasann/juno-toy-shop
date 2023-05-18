4038c71a9cc4d3cb6531e86f833f2af9b61a7a2f
afnafmafeee <afnafmafeee@gmail.com>
2023-05-19 00:14:12 +0600

    first commit

---
+++ src/Pages/SharedPage/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import Logo from '../../../assets/Logo/logo.webp'
+import { XMarkIcon, Bars3CenterLeftIcon } from "@heroicons/react/24/solid";
+import './Header.css'
+const Header = () => {
+    const [openMenu, setOpenMenu] = useState(false)
+    
+    return (
+     
+    );
+};
+
+export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Layout from "./Layout/Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
