8f0c093d2a8229e01968c856fc3e7bb0cd41aa2c
afnafmafeee <afnafmafeee@gmail.com>
2023-05-19 01:19:18 +0600

    banner section done

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
+import Img from '../../../assets/Banner/creative-image-1-copyright.webp'
 
 const Banner = () => {
     return (
       <div className="my_container">
-        <div>
+        <div className='lg:flex lg:justify-between lg:items-center'>
           {/* Text Area */}
-          <div>
-            <h1>Best Toy For Your children</h1>
-            <p>lorem</p>
-            <button className="button ">
-              All Toys{" "}
-            </button>
+          <div className='lg:space-y-6 text-center  lg:text-start space-y-5 lg:space-y-0 '>
+            <h1 className="font-bold text-5xl lg:text-6xl text-[#181d4e] lg:leading-[1.14]">
+              Find your best<br className="hidden lg:block"></br> Toys for your
+              <br className="hidden lg:block"></br> Children
+            </h1>
+            <p className="text-xl text-gray-500 ">
+              We deliver best of fantastic,hand-piched,age
+              <br className="hidden lg:block"></br> appropriate toys doll to
+              your door.
+            </p>
+            <button className="button ">All Toys </button>
           </div>
           {/* Image Area */}
-          <div>
-            <img src="" alt="" />
+          <div className='lg:w-1/2 mx-auto lg:mx-0 mt-5 lg:mt-0  '>
+            <img src={Img} alt="" />
           </div>
         </div>
       </div>
