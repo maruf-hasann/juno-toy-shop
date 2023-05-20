e2f58314ded1f5e271d10fb3f76ff96c9793d0a1
afnafmafeee <afnafmafeee@gmail.com>
2023-05-20 13:57:53 +0600

    sorting add

--- src/Pages/SharedPage/Header/Header.jsx
+++ src/Pages/SharedPage/Header/Header.jsx
@@ -137,8 +137,10 @@ const Header = () => {
                       <li>
                         <NavLink to="/blogs">Blogs</NavLink>
                       </li>
+                      
                     </ul>
                   </nav>
+                  
                 </div>
               </div>
             )}
